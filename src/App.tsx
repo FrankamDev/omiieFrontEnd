8e62af5a22a7a3c80ea0e1e36d60067e75f2f7c9
FrankamDev <fkamgang06@gmail.com>
2026-06-28 00:11:26 +0200

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
